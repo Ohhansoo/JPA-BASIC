9c868190ae1d5b6b6c3f5eb9ce6ea988eb7ad782
hansoo <zhdnrk124@naver.com>
2024-02-20 07:01:27 +0900

    [JPA-기본매핑] init commit

---
+++ src/main/java/jpabook/Main.java
@@ -0,0 +1,23 @@
+package jpabook;
+
+import javax.persistence.*;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
